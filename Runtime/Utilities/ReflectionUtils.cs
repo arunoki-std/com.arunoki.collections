7f95474f35baac3e84d64b7e5ef42909b7473314
arunoki-std <arunoki.studio@gmail.com>
2026-01-06 18:47:43 +0200

    cleanup

--- Runtime/Utilities/ReflectionUtils.cs
+++ Runtime/Utilities/ReflectionUtils.cs
@@ -77,54 +77,5 @@ namespace Arunoki.Collections.Utilities
 
       return values;
     }
-
-    #region Obsolete
-
-    // public static List<T> GetAllPropertiesWithNested<T> (this object source, BindingFlags flags = PublicFlags)
-    // {
-    //   var result = new List<T> ();
-    //   GetNestedTypes (source, result, flags);
-    //   return result;
-    // }
-    //
-    // private static void GetNestedTypes<T> (object source, List<T> collection, BindingFlags flags)
-    // {
-    //   foreach (var obj in source.GetAllProperties<T> (flags))
-    //   {
-    //     if (!collection.Contains (obj))
-    //     {
-    //       collection.Add (obj);
-    //       GetNestedTypes (obj, collection, flags);
-    //     }
-    //   }
-    // }
-
-    //TODO: optimisation - cache PropertyInfo
-    // public static List<T> GetAllProperties<T> (this object source, BindingFlags flags = PublicFlags)
-    // {
-    //   var lookingType = typeof(T);
-    //   var isType = source is Type;
-    //   var sourceType = isType ? source as Type : source.GetType ();
-    //   var sourceObject = isType ? null : source;
-    //   if (isType) flags |= BindingFlags.Static; // fix issue with static singleton field
-    //
-    //   var properties = sourceType.GetProperties (flags);
-    //   var values = new List<T> (properties.Length);
-    //
-    //   for (int index = 0; index < properties.Length; index++)
-    //   {
-    //     var property = properties [index];
-    //
-    //     if (property.PropertyType == lookingType || lookingType.IsAssignableFrom (property.PropertyType))
-    //     {
-    //       var value = (T) property.GetValue (sourceObject);
-    //       if (value != null) values.Add (value);
-    //     }
-    //   }
-    //
-    //   return values;
-    // }
-
-    #endregion
   }
 }
